e6e6f719b0d053455eef3e4042fa0cf3ac24a6cd
DevArpan7 <72331581+Arpan-Chanda@users.noreply.github.com>
2021-07-19 13:53:14 +0530

    Admin dashboard, cutomer section route and component created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
-const routes: Routes = [];
+import { DashboardComponent } from "./component/admin/dashboard/dashboard.component";
+import { CustomerListComponent } from "./component/admin/customer/customer-list/customer-list.component";
+import { CustomerAddComponent } from "./component/admin/customer/customer-add/customer-add.component";
+const routes: Routes = [
+  {path : '', component : DashboardComponent, pathMatch:'full'},
+  {path : 'dashboard', component : DashboardComponent},
+  {path : 'customer', children: [
+      {path: 'list', component: CustomerListComponent},
+      {path: 'add', component: CustomerAddComponent}
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './component/admin/layouts/header/header.component';
+import { FooterComponent } from './component/admin/layouts/footer/footer.component';
+import { SidebarComponent } from './component/admin/layouts/sidebar/sidebar.component';
+import { DashboardComponent } from './component/admin/dashboard/dashboard.component';
+import { CustomerListComponent } from './component/admin/customer/customer-list/customer-list.component';
+import { CustomerAddComponent } from './component/admin/customer/customer-add/customer-add.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    SidebarComponent,
+    DashboardComponent,
+    CustomerListComponent,
+    CustomerAddComponent
   ],
   imports: [
     BrowserModule,
