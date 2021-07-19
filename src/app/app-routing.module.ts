e07f00e219f987618a26cb25a99ed01e5303b43e
DevArpan7 <72331581+Arpan-Chanda@users.noreply.github.com>
2021-07-19 14:34:05 +0530

    Admin tickets, products, reports section route and component created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,14 +3,45 @@ import { Routes, RouterModule } from '@angular/router';
 import { DashboardComponent } from "./component/admin/dashboard/dashboard.component";
 import { CustomerListComponent } from "./component/admin/customer/customer-list/customer-list.component";
 import { CustomerAddComponent } from "./component/admin/customer/customer-add/customer-add.component";
+import { CustomerDetailComponent } from "./component/admin/customer/customer-detail/customer-detail.component";
+import { TicketListComponent } from "./component/admin/ticket/ticket-list/ticket-list.component";
+import { TicketDetailComponent } from "./component/admin/ticket/ticket-detail/ticket-detail.component";
+import { ProductListComponent } from "./component/admin/product/product-list/product-list.component";
+import { ProductDetailComponent } from "./component/admin/product/product-detail/product-detail.component";
+import { CustomerReportListComponent } from "./component/admin/report/customer-report/customer-report-list/customer-report-list.component";
+import { CustomerReportDeatilComponent } from "./component/admin/report/customer-report/customer-report-deatil/customer-report-deatil.component";
+import { ProductReportListComponent } from "./component/admin/report/product-report/product-report-list/product-report-list.component";
+import { ProductReportDetailComponent } from "./component/admin/report/product-report/product-report-detail/product-report-detail.component";
+import { TicketReportListComponent } from "./component/admin/report/ticket-report/ticket-report-list/ticket-report-list.component";
+import { TicketReportDetailComponent } from "./component/admin/report/ticket-report/ticket-report-detail/ticket-report-detail.component";
 const routes: Routes = [
   {path : '', component : DashboardComponent, pathMatch:'full'},
   {path : 'dashboard', component : DashboardComponent},
   {path : 'customer', children: [
-      {path: 'list', component: CustomerListComponent},
-      {path: 'add', component: CustomerAddComponent}
-    ],
-  },
+    {path: 'list', component: CustomerListComponent},
+    {path: 'add', component: CustomerAddComponent},
+    {path: 'detail/:customerId', component: CustomerDetailComponent},
+    {path: 'report', children: [
+      {path: 'list', component: CustomerReportListComponent},
+      {path: 'detail/:customerId', component: CustomerReportDeatilComponent},
+    ]}
+  ]},
+  {path: 'ticket', children: [
+    {path: 'list', component: TicketListComponent},
+    {path: 'detail/:ticketId', component: TicketDetailComponent},
+    {path: 'report', children: [
+      {path: 'list', component: TicketReportListComponent},
+      {path: 'detail/:ticketId', component: TicketReportDetailComponent},
+    ]}
+  ]},
+  {path: 'product', children: [
+    {path: 'list', component: ProductListComponent},
+    {path: 'detail/:productId', component: ProductDetailComponent},
+    {path: 'report', children: [
+      {path: 'list', component: ProductReportListComponent},
+      {path: 'detail/:productId', component: ProductReportDetailComponent},
+    ]}
+  ]},
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,17 @@ import { SidebarComponent } from './component/admin/layouts/sidebar/sidebar.comp
 import { DashboardComponent } from './component/admin/dashboard/dashboard.component';
 import { CustomerListComponent } from './component/admin/customer/customer-list/customer-list.component';
 import { CustomerAddComponent } from './component/admin/customer/customer-add/customer-add.component';
+import { TicketListComponent } from './component/admin/ticket/ticket-list/ticket-list.component';
+import { CustomerDetailComponent } from './component/admin/customer/customer-detail/customer-detail.component';
+import { TicketDetailComponent } from './component/admin/ticket/ticket-detail/ticket-detail.component';
+import { ProductListComponent } from './component/admin/product/product-list/product-list.component';
+import { ProductDetailComponent } from './component/admin/product/product-detail/product-detail.component';
+import { CustomerReportListComponent } from './component/admin/report/customer-report/customer-report-list/customer-report-list.component';
+import { CustomerReportDeatilComponent } from './component/admin/report/customer-report/customer-report-deatil/customer-report-deatil.component';
+import { TicketReportListComponent } from './component/admin/report/ticket-report/ticket-report-list/ticket-report-list.component';
+import { TicketReportDetailComponent } from './component/admin/report/ticket-report/ticket-report-detail/ticket-report-detail.component';
+import { ProductReportListComponent } from './component/admin/report/product-report/product-report-list/product-report-list.component';
+import { ProductReportDetailComponent } from './component/admin/report/product-report/product-report-detail/product-report-detail.component';
 
 @NgModule({
   declarations: [
@@ -18,7 +29,18 @@ import { CustomerAddComponent } from './component/admin/customer/customer-add/cu
     SidebarComponent,
     DashboardComponent,
     CustomerListComponent,
-    CustomerAddComponent
+    CustomerAddComponent,
+    TicketListComponent,
+    CustomerDetailComponent,
+    TicketDetailComponent,
+    ProductListComponent,
+    ProductDetailComponent,
+    CustomerReportListComponent,
+    CustomerReportDeatilComponent,
+    TicketReportListComponent,
+    TicketReportDetailComponent,
+    ProductReportListComponent,
+    ProductReportDetailComponent
   ],
   imports: [
     BrowserModule,
