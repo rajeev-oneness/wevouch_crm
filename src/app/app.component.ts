9935319710e4a37d76d88fb9f7bc678b417425ea
DevArpan7 <72331581+Arpan-Chanda@users.noreply.github.com>
2021-07-21 11:02:07 +0530

    Changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavigationStart, Router } from "@angular/router";
+import { NgxUiLoaderService } from 'ngx-ui-loader';
 
 @Component({
   selector: 'app-root',
@@ -10,7 +11,7 @@ export class AppComponent {
   title = 'wevouch';
   public showHeaderFooterSidebar: boolean = false;
 
-  constructor(private _router:Router) {
+  constructor(private _router:Router, private _loader: NgxUiLoaderService) {
     _router.events.forEach((event) => {
       if (event instanceof NavigationStart) {
         if (event['url'] == '/login' || event['url'] == '/registration') {
