29f513332d28f0a5b7a7fcd4e58fcca445266fbf
DevArpan7 <72331581+Arpan-Chanda@users.noreply.github.com>
2021-07-23 14:54:01 +0530

    Category section

--- src/app/component/admin/master/category/category-add/category-add.component.ts
+++ src/app/component/admin/master/category/category-add/category-add.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from 'src/app/service/api.service';
+import { NgxUiLoaderService } from 'ngx-ui-loader';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-category-add',
@@ -7,9 +10,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoryAddComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _api:ApiService,private _loader : NgxUiLoaderService,private _router:Router) {
+    this._loader.startLoader('loader');
+   }
+  public errorMessage = '';
 
   ngOnInit(): void {
+    this._loader.stopLoader('loader');
+  }
+
+  categoryFormSubmit(formData){
+    this.errorMessage = '';
+    for( let i in formData.controls ){
+      formData.controls[i].markAsTouched();
+    }
+    if( formData?.valid ){
+      const mainForm = formData.form.value;
+      this._loader.startLoader('loader');
+      this._api.categoryCreate(mainForm).subscribe(
+        res => {
+          console.log(res);
+          this.errorMessage = res.message;
+          this._loader.stopLoader('loader');
+          this._router.navigate(['/admin/master/category/list']);
+        },
+        err => {
+          console.log(err.message)
+          this.errorMessage = err.message;
+          this._loader.stopLoader('loader');
+        }
+        
+      )
+    }else{
+      this.errorMessage = 'Please fill out all the details';
+    }
+    // console.log('Form Data SUbmitted');
   }
 
 }
--- src/app/component/admin/master/category/category-edit/category-edit.component.ts
+++ src/app/component/admin/master/category/category-edit/category-edit.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { NgxUiLoaderService } from 'ngx-ui-loader';
+import { ApiService } from 'src/app/service/api.service';
+import { Router } from "@angular/router";
 
 @Component({
   selector: 'app-category-edit',
@@ -7,9 +11,61 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoryEditComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private _loader : NgxUiLoaderService,private _api:ApiService,private _activated:ActivatedRoute,private _router: Router) { 
+    this._loader.startLoader('loader');
+  }
+  
+  public categoryId : any = 0;
+  public categoryDetail: any = {};
+  public errorMessage: any = '';
   ngOnInit(): void {
+    this.categoryId = this._activated.snapshot.paramMap.get('categoryId');
+    this.getCategoryDetails(this.categoryId);
+  }
+
+  getCategoryDetails(categoryId) {
+    this._loader.startLoader('loader');
+    this._api.categoryDetail(categoryId).subscribe(
+      res => {
+        console.log(res);
+        this.categoryDetail = res;
+        this._loader.stopLoader('loader');
+      }, err => {}
+    )
+  }
+
+  categoryFormSubmit(formData){
+    console.log(this.categoryId);
+    console.log(formData);
+    
+    this.errorMessage = '';
+    for( let i in formData.controls ){
+      formData.controls[i].markAsTouched();
+    }
+    if( formData?.valid ){
+      // console.log(this.categoryId);
+      
+      const mainForm = formData.form.value;
+      this._loader.startLoader('loader');
+      this._api.categoryUpdate(mainForm,this.categoryId).subscribe(
+        res => {
+          console.log(res);
+          this.errorMessage = res.message;
+          this._loader.stopLoader('loader');
+          this._router.navigate(['/admin/master/category/list']);
+        },
+        err => {
+          console.log(err.message)
+          this.errorMessage = err.message;
+          this._loader.stopLoader('loader');
+        }
+        
+      )
+    }
+    else{
+      this.errorMessage = 'Please fill out all the details';
+    }
+    // console.log('Form Data SUbmitted');
   }
 
 }
