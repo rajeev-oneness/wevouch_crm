f4af75a0439096e0fb2afaf940d03fe20ce59d11
DevArpan7 <72331581+Arpan-Chanda@users.noreply.github.com>
2021-07-23 10:55:43 +0530

    login, dashboard api integrated

--- src/app/component/admin/dashboard/dashboard.component.ts
+++ src/app/component/admin/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from 'src/app/service/api.service';
 import { NgxUiLoaderService } from 'ngx-ui-loader';
 
 @Component({
@@ -8,10 +9,42 @@ import { NgxUiLoaderService } from 'ngx-ui-loader';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor(private _loader: NgxUiLoaderService) { }
-  
-  ngOnInit(): void {
-    
+  // public dashboardData : {data : DASHBOARD[]};
+  constructor(private _api:ApiService, private _loader:NgxUiLoaderService) { 
+    this._loader.startLoader('loader');
+    // this.dashboardData = {data : []};
   }
 
+  ngOnInit(): void {
+    this.getpPackageList();
+  }
+
+  getpPackageList() {
+    // this.dashboardData.data = [];
+    this._loader.startLoader('loader');
+    this._api.dashboardData().subscribe(
+      res => {
+        console.log(res);
+        // this.dashboardData.data = res;
+        this.dashboard = res;
+        // console.log(this.dashboardData.data)
+        // this.latestTickets = res.latestTickets;
+        this._loader.stopLoader('loader');
+      },err => {} 
+    )
+  }
+        // this.dashboardData.data = res;
+
+  public dashboard :any = [];
+
 }
+
+// interface DASHBOARD{
+//   totalCustomers : number,
+//   totalNewCustomers : number,
+//   totalProducts : number,
+//   totalNewProducts : number,
+//   totalTickets : number,
+//   totalNewTickets : number,
+//   latestTickets : any,
+// }
